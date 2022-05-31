53b1a925f28a3793f70409ee8201950ebf8ab6b5
Eilon Lipton <Eilon@users.noreply.github.com>
2022-05-31 14:32:41 -0700

    Initial commit

---
+++ MauiSignalRGridDemo/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace MauiSignalRGridDemo;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new MainPage();
+	}
+}
---
+++ MauiSignalRGridDemo/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace MauiSignalRGridDemo;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
